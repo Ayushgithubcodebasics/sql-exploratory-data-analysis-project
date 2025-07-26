92ec5b67dd7b7c51d76eb9d9e86dc585144516c4
Ayushgithubcodebasics <155321593+Ayushgithubcodebasics@users.noreply.github.com>
2025-07-26 16:55:25 +0300

    Update 06_ranking_analysis.sql

--- scripts/06_ranking_analysis.sql
+++ scripts/06_ranking_analysis.sql
@@ -1,7 +1,6 @@
 /*
-===============================================================================
 Ranking Analysis
-===============================================================================
+
 Purpose:
     - To rank items (e.g., products, customers) based on performance or other metrics.
     - To identify top performers or laggards.
@@ -9,7 +8,6 @@ Purpose:
 SQL Functions Used:
     - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
     - Clauses: GROUP BY, ORDER BY
-===============================================================================
 */
 
 -- Which 5 products Generating the Highest Revenue?
@@ -18,8 +16,8 @@ SELECT TOP 5
     p.product_name,
     SUM(f.sales_amount) AS total_revenue
 FROM gold.fact_sales f
-LEFT JOIN gold.dim_products p
-    ON p.product_key = f.product_key
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
 GROUP BY p.product_name
 ORDER BY total_revenue DESC;
 
@@ -31,8 +29,8 @@ FROM (
         SUM(f.sales_amount) AS total_revenue,
         RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
     FROM gold.fact_sales f
-    LEFT JOIN gold.dim_products p
-        ON p.product_key = f.product_key
+        LEFT JOIN gold.dim_products p
+            ON p.product_key = f.product_key
     GROUP BY p.product_name
 ) AS ranked_products
 WHERE rank_products <= 5;
@@ -42,8 +40,8 @@ SELECT TOP 5
     p.product_name,
     SUM(f.sales_amount) AS total_revenue
 FROM gold.fact_sales f
-LEFT JOIN gold.dim_products p
-    ON p.product_key = f.product_key
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
 GROUP BY p.product_name
 ORDER BY total_revenue;
 
@@ -54,8 +52,8 @@ SELECT TOP 10
     c.last_name,
     SUM(f.sales_amount) AS total_revenue
 FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-    ON c.customer_key = f.customer_key
+    LEFT JOIN gold.dim_customers c
+        ON c.customer_key = f.customer_key
 GROUP BY 
     c.customer_key,
     c.first_name,
@@ -69,10 +67,10 @@ SELECT TOP 3
     c.last_name,
     COUNT(DISTINCT order_number) AS total_orders
 FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-    ON c.customer_key = f.customer_key
+    LEFT JOIN gold.dim_customers c
+        ON c.customer_key = f.customer_key
 GROUP BY 
     c.customer_key,
     c.first_name,
     c.last_name
-ORDER BY total_orders ;
+ORDER BY total_orders;
