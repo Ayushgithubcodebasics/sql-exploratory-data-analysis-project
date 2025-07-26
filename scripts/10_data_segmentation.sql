8aeef67a1820b96bd8a79c787847fdd3da4df184
Ayushgithubcodebasics <155321593+Ayushgithubcodebasics@users.noreply.github.com>
2025-07-26 16:58:17 +0300

    Update 10_data_segmentation.sql

--- scripts/10_data_segmentation.sql
+++ scripts/10_data_segmentation.sql
@@ -1,7 +1,6 @@
 /*
-===============================================================================
 Data Segmentation Analysis
-===============================================================================
+
 Purpose:
     - To group data into meaningful categories for targeted insights.
     - For customer segmentation, product categorization, or regional analysis.
@@ -9,11 +8,10 @@ Purpose:
 SQL Functions Used:
     - CASE: Defines custom segmentation logic.
     - GROUP BY: Groups data into segments.
-===============================================================================
 */
 
 /*Segment products into cost ranges and 
-count how many products fall into each segment*/
+  count how many products fall into each segment*/
 WITH product_segments AS (
     SELECT
         product_key,
@@ -35,21 +33,21 @@ GROUP BY cost_range
 ORDER BY total_products DESC;
 
 /*Group customers into three segments based on their spending behavior:
-	- VIP: Customers with at least 12 months of history and spending more than €5,000.
-	- Regular: Customers with at least 12 months of history but spending €5,000 or less.
-	- New: Customers with a lifespan less than 12 months.
-And find the total number of customers by each group
+    - VIP: Customers with at least 12 months of history and spending more than €5,000.
+    - Regular: Customers with at least 12 months of history but spending €5,000 or less.
+    - New: Customers with a lifespan less than 12 months.
+  And find the total number of customers by each group
 */
 WITH customer_spending AS (
     SELECT
         c.customer_key,
-        SUM(f.sales_amount) AS total_spending,
-        MIN(order_date) AS first_order,
-        MAX(order_date) AS last_order,
-        DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+        SUM(f.sales_amount)                                     AS total_spending,
+        MIN(order_date)                                         AS first_order,
+        MAX(order_date)                                         AS last_order,
+        DATEDIFF(month, MIN(order_date), MAX(order_date))      AS lifespan
     FROM gold.fact_sales f
-    LEFT JOIN gold.dim_customers c
-        ON f.customer_key = c.customer_key
+        LEFT JOIN gold.dim_customers c
+            ON f.customer_key = c.customer_key
     GROUP BY c.customer_key
 )
 SELECT 
